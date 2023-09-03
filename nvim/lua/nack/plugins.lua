dc442ec98eb9a1ec7be4230fcffc14501e0dfbd7
nack098 <77537263+nack098@users.noreply.github.com>
2023-09-03 13:05:59 +0700

    fix(sourcing-luasnip): source of luasnip not working

--- nvim/lua/nack/plugins.lua
+++ nvim/lua/nack/plugins.lua
@@ -51,7 +51,11 @@ require("lazy").setup({
     end,
   },
   {
-    'L3MON4D3/LuaSnip',
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
     dependencies = { 'rafamadriz/friendly-snippets' }
   },
   {
