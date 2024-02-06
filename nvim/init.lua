e9981d177988efd5c98c93bbb998db9438436bd9
nack098 <77537263+nack098@users.noreply.github.com>
2024-02-07 06:28:34 +0700

    feat(nvim-config): update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,2 @@
-require('nack.base')
-require('nack.highlights')
-require('nack.maps')
-require('nack.plugins')
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_win = has "win32"
-local is_linux = has "linux"
-
-if is_mac then
-  require('nack.macos')
-end
-
-if is_win then
-  require('nack.windows')
-end
-
-if is_linux then
-  require('nack.linux')
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/lua/plugins/notify.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = function()
+      return {
+        background_colour = "#000000",
+      }
+    end,
+  },
+}
