1c1542cae816ef2a4e4edd3409bb9c421f011127
nack098 <77537263+nack098@users.noreply.github.com>
2024-03-15 15:14:04 +0700

    Clear commit

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,6 +3,10 @@
 -- Add any additional options here
 
 vim.opt.mouse = ""
+vim.opt.tabstop = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 4
 
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
